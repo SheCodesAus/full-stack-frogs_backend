2cef2bcadaf41f74b9a26510491a1dff549e9b8b
gudetamago <sharakael@yahoo.com>
2025-11-15 20:07:25 +0800

    feat: Added model for PulseLog, Mood and Workload

--- teampulse/pulse_logs/models.py
+++ teampulse/pulse_logs/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class PulseLog(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        related_name='logged_pulses',
+        on_delete=models.CASCADE
+    )
+    mood = models.ForeignKey('Mood', on_delete=models.CASCADE)
+    workload = models.ForeignKey('Workload', on_delete=models.CASCADE)
+    comment = models.TextField()
+    # Team is not linked to Team model as it's write-only
+    team = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Mood(models.Model):
+    value = models.IntegerField()
+    description = models.CharField(max_length=255)
+    image_url = models.URLField()
+
+class Workload(models.Model):
+    value = models.IntegerField()
+    description = models.CharField(max_length=255)
+    image_url = models.URLField()
