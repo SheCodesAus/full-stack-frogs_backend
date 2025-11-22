e9d9b87a72fbd3a1ffdd5a286e87516044605eb9
TRANGVO <trangvo24267@gmail.com>
2025-11-22 10:41:25 +0800

    22nov25-1

--- teampulse/users/views.py
+++ teampulse/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Team #'.models' means the models.py in the same branch
+from .serializers import TeamSerializer
 
-# Create your views here.
+class TeamList(APIView):
+
+    def get(self, request):
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TeamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
