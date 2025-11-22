9e3fbfe536ab99bd38194cf389a667e39d157bd1
gudetamago <31135745+gudetamago@users.noreply.github.com>
2025-11-22 11:48:50 +0800

    Revert "22nov25-1"
    This reverts commit e9d9b87a72fbd3a1ffdd5a286e87516044605eb9.

--- teampulse/users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Team
-
-class TeamSerializer(serializers.TeamSerializer):
-    class Meta:
-        model = apps.get_model('users.Team')
-        fields = '__all__'
--- teampulse/users/views.py
+++ teampulse/users/views.py
@@ -1,20 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import Http404
-from .models import Team #'.models' means the models.py in the same branch
-from .serializers import TeamSerializer
+from django.shortcuts import render
 
-class TeamList(APIView):
-
-    def get(self, request):
-        teams = Team.objects.all()
-        serializer = TeamSerializer(teams, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = TeamSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Create your views here.
