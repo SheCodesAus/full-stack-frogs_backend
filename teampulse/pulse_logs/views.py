553595e2a934fcc07b13d317c272248082d466c8
gudetamago <31135745+gudetamago@users.noreply.github.com>
2025-11-22 12:12:16 +0800

    Reapply "Merge pull request #2 from SheCodesAus/Add-API-for-Moods"
    This reverts commit 89770d53f07644fc3b1cec5197f25a3bce024d7e.

--- teampulse/pulse_logs/views.py
+++ teampulse/pulse_logs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from django.http import Http404
+from django.db.models import Sum
+from .models import PulseLog, Mood, Workload
+from .serializers import MoodSerializer
+# from .permissions import IsOwnerOrReadOnly, isStaffOrReadOnly
 
-# Create your views here.
+class MoodList(APIView):
+
+    def get(self, request):
+        moods = Mood.objects.all()
+        serializer = MoodSerializer(moods, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MoodSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
