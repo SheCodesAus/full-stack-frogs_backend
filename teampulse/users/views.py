61b237fbda8a48b7fbd6492d50029f305c548c51
TRANGNVO <trangvo24267@gmail.com>
2025-11-23 19:05:03 +0800

    feat: Added Teams API calls

--- teampulse/users/views.py
+++ teampulse/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from django.http import Http404
+from .models import Team
+from .serializers import TeamSerializer
+# from .permissions import IsOwnerOrReadOnly, isStaffOrReadOnly
 
-# Create your views here.
+class TeamList(APIView):
+
+    def get(self, request):
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TeamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
