2c36339350c828ba65717630a75dde2274f71fae
gudetamago <31135745+gudetamago@users.noreply.github.com>
2025-11-23 19:01:00 +0800

    fix: Testing ruleset

--- teampulse/pulse_logs/urls.py
+++ teampulse/pulse_logs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('moods/', views.MoodList.as_view()),
-    path('workloads/', views.WorkloadList.as_view())
+    path('workloads/', views.WorkloadList.as_view()),
+
 ]
