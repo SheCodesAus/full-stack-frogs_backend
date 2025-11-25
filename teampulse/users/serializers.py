29c5860915159701d9352160ddd3051b8b01f35e
TRANGVO <trangvo24267@gmail.com>
2025-11-25 19:50:30 +0800

    USERS API - ADMIN ADDED

--- teampulse/users/serializers.py
+++ teampulse/users/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
 from django.apps import apps
+from .models import CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+        extra_kwargs = {'password': { 'write_only': True }}     #kwargs - keyword arguments #one cannot read password
+
+    def create(self, validated_data): #validated - once serializers validate data, it will pass them into function so that it is ensured the data are valid
+
+        return CustomUser.objects.create_user(**validated_data)
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
--- teampulse/users/views.py
+++ teampulse/users/views.py
@@ -3,7 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.http import Http404
 from .models import Team
+from .models import CustomUser
 from .serializers import TeamSerializer
+from .serializers import CustomUserSerializer
+
 # from .permissions import IsOwnerOrReadOnly, isStaffOrReadOnly
 
 class TeamList(APIView):
@@ -19,3 +22,53 @@ class TeamList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CustomUserDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user) #we donot need many anymore, only unique instance is required
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        print(f"updating: {pk}")
+        user = self.get_object(pk) #giving the instance to the "serializers"-instance
+        serializer = CustomUserSerializer(
+            instance=user,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+class CustomUserList(APIView):
+    def get(self, request): #getting a list of users
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request): #ability to create new users
+        serializer = CustomUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
