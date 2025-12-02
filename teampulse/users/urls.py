357fc3280844f7303fffff3b16ca267153681477
TRANGVO <trangvo24267@gmail.com>
2025-12-02 19:21:22 +0800

    TEAMS PUT API

--- teampulse/users/urls.py
+++ teampulse/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('teams/<int:pk>/', views.TeamDetail.as_view()),
     path('teams/', views.TeamList.as_view()),
     path('users/', views.CustomUserList.as_view()),
     path('users/<int:pk>/', views.CustomUserDetail.as_view())
--- teampulse/users/views.py
+++ teampulse/users/views.py
@@ -10,6 +10,35 @@ from .serializers import TeamSerializer, CustomUserSerializer
 
 # from .permissions import IsOwnerOrReadOnly, isStaffOrReadOnly
 
+class TeamDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Team.objects.get(pk=pk)
+        except Team.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        team = self.get_object(pk)
+        serializer = TeamSerializer(team) #we donot need many anymore, only unique instance is required
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        print(f"updating: {pk}")
+        team = self.get_object(pk) #giving the instance to the "serializers"-instance
+        serializer = TeamSerializer(
+            instance=team,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )   
+
 class TeamList(APIView):
 
     def get(self, request):
